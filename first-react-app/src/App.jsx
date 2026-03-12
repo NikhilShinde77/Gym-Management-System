b170a819c18019a5b4dfd7dd714716e6a8df7665
NikhilShinde77 <nikhilshinde15651@gmail.com>
2026-03-12 22:05:12 +0530

    Updated React app: Tailwind setup and UI changes

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <h1>hi my name is nikhil</h1>
-      
+    hi my name is nikhil
     </>
-  )
+   
+  );
 }
 
-export default App
+export default App;
